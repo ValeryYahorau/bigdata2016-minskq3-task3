a6e4ce32c4a687fcc27865661a9d3d593fd36d05
ValeryYahorau <yehorav.valery@gmail.com>
2016-09-02 00:45:57 +0300

    Second task. Custom type implementation.

--- src/main/java/com/epam/bigdata2016/minskq3/task3/VisitsSpendsCount.java
+++ src/main/java/com/epam/bigdata2016/minskq3/task3/VisitsSpendsCount.java
@@ -6,13 +6,12 @@ import java.util.regex.Pattern;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
-import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
-import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapreduce.lib.output.SequenceFileOutputFormat;
 import org.apache.hadoop.util.GenericOptionsParser;
 
 public class VisitsSpendsCount {
@@ -83,8 +82,10 @@ public class VisitsSpendsCount {
         job.setReducerClass(VisitsSpendsReducer.class);
         job.setOutputKeyClass(Text.class);
         job.setOutputValueClass(VisitSpendComparable.class);
+        job.setOutputFormatClass(SequenceFileOutputFormat.class);
+
         FileInputFormat.addInputPath(job, new Path(otherArgs[0]));
-        FileOutputFormat.setOutputPath(job, new Path(otherArgs[1]));
+        SequenceFileOutputFormat.setOutputPath(job, new Path(otherArgs[1]));
 
         System.exit(job.waitForCompletion(true) ? 0 : 1);
     }
